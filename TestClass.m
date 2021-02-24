6f10e2e24e8838014f2d1456885364f6003a729c
aroide <aroide@gmail.com>
2021-02-24 22:55:24 +0900

    [TEST] - 첫 커밋

---
+++ TestClass.m
@@ -0,0 +1,25 @@
+//
+//  TestClass.m
+//  testprj
+//
+//  Created by 김택훈 on 14/05/2019.
+//  Copyright © 2019 김택훈. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+- (id)initWithTest:(NSString *)test {
+    
+    self = [super init];
+    
+    return self;
+}
+
+- (void)testRequiredProtocol {
+    NSLog(@"testRequiredProtocol");
+}
+
+
+@end
